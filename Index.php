65eb1174e7ab0f7cb999e4ee05a0f8a3b3f7926f
SwannLV <le.vigouroux.swann@gmail.com>
2015-05-26 14:35:18 +0000

    plane geometry

--- Index.php
+++ Index.php
@@ -48,13 +48,17 @@
               'awe.marker_ar.js',
             ],
             success: function() { 
-							awe.setup_scene();			
+					awe.setup_scene();			
 			        awe.pois.add({ id:'poi_1', position: { x:0, y:0, z:0 }, visible: true });
 			        awe.projections.add({ 
-			          id:'projection_1', 
-								geometry: { shape: 'cube', x:400, y:400, z:1 },
-			          material:{ type: 'phong', color: 0xFFFFFF, transparent: true }, 
-                texture: { path: 'pasta.png' },
+                        id:'projection_1', 
+                        //geometry: { shape: 'planegeometry', width:400, height:400 },//
+                        //geometry:{ shape: 'cube', x:330, y:0.5, z:300 },
+                        geometry:{ shape: 'plane', height:300, width:330 },
+                        material:{ type: 'phong', color: 0xFFFFFF, transparent: true }, 
+                        texture: { path: 'inverted-pasta.png' },
+                        rotation:{ x:45, y:0, z:0 }
+                        //rotation:{ x:0, y:0, z:0 }
 			        }, { poi_id: 'poi_1' });
 			        awe.events.add([{
 								id: 'ar_tracking_marker',
@@ -73,8 +77,8 @@
 										if (event.detail['64']) { // we are mapping marker #64 to this projection
 											awe.pois.update({
 												data: {
-													visible: true,
-                          position: { x:0, y:0, z:0 },
+												    visible: true,
+                                                    position: { x:0, y:0, z:100 },
 													matrix: event.detail['64'].transform
 												},
 												where: {
